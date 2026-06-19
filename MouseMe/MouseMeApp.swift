60f76f2256a4c41a44220287957d25f65a405cb3
Ryan <Fermoselle>
2026-06-18 21:03:22 -0700

    Initial Commit

---
+++ MouseMe/MouseMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseMeApp.swift
+//  MouseMe
+//
+//  Created by Ryan Joshua Fermoselle on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct MouseMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseMeTests/MouseMeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MouseMeTests.swift
+//  MouseMeTests
+//
+//  Created by Ryan Joshua Fermoselle on 6/18/26.
+//
+
+import Testing
+
+struct MouseMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
